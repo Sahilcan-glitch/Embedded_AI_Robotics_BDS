d694c87c553d75939d08dc888ea0b7a15e9d1563
Sahil <140575243+Sahilcan-glitch@users.noreply.github.com>
2025-11-28 04:11:29 +1100

    Create server.py

---
+++ week03_streamlit_chatbot_and_web_control/code_example_flask/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+messages = []
+
+@app.route("/send", methods=["POST"])
+def send():
+    data = request.json
+    messages.append(data)
+    return jsonify({"status": "received", "data": data})
+
+@app.route("/get", methods=["GET"])
+def get():
+    return jsonify(messages)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
